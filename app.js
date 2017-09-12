9580b8f894417e901ad94aa4cb0fe678c5846103
Matt Oskamp <mattoskamp@gmail.com>
2017-09-12 10:26:59 -0400

    forcing https

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+var sslRedirect = require('heroku-ssl-redirect');
 var express = require('express');
 var routes = require('./app/routes');
 var app = express();
 app.set('view engine', 'pug');
+
+// enable ssl redirect
+app.use(sslRedirect());
+
 
 app.use(express.static('public'));
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap-wysiwyg/js/'));
