fc4a1b135831ebb30acb6e9497d6894ce848e21a
Matt Oskamp <mattoskamp@gmail.com>
2017-09-15 22:31:16 -0400

    giving the bot life

--- app.js
+++ app.js
@@ -4,11 +4,19 @@ var express = require('express');
 var routes = require('./app/routes');
 var facebook = require('./app/facebook');
 var socketEvents = require('./app/socketEvents');
+var bodyParser = require('body-parser');
+
 var app = express();
 app.set('view engine', 'pug');
 
 // enable ssl redirect
 app.use(sslRedirect());
+
+// body-parser
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.use(express.static('public'));
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap-wysiwyg/js/'));
